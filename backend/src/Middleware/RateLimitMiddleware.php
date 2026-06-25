8c8bd411a894f8bd2b05dcbd4d861ff57f32e5ed
kdairutt <kaipek914@gmail.com>
2026-06-25 15:46:08 +0300

    feat: configure RateLimitMiddleware.php, delete invalid composer files in root directory and move them to backend

--- backend/src/Middleware/RateLimitMiddleware.php
+++ backend/src/Middleware/RateLimitMiddleware.php
@@ -1,8 +1,30 @@
 <?php 
     namespace Matchla\Middleware;
+    
+    use Matchla\Config\Cache;
 
     class RateLimitMiddleware {
         public function handle(): bool {
+            $redis = Cache::getInstance()->getRedis();
+
+            $key = "rate_limit:" . $_SERVER["REMOTE_ADDR"];
+            
+
+            # rate key'ini oluştur ve rate'i bir artır
+            $rate =$redis->incr($key);
+            
+            if($rate === 1) {
+                $redis->expire($key, 60);
+            }
+
+            if($rate > 60) {
+                http_response_code(429);
+                echo json_encode([
+                    "error" => "too many requests"
+                ]);
+                return false;
+            }
+
             return true;
         }
     }
